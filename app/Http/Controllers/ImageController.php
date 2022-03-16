5cc311595cb52f862a064074eb342b49270c7259
akramk295 <75097000+akramk295@users.noreply.github.com>
2022-03-16 14:40:44 +0530

    update

--- app/Http/Controllers/ImageController.php
+++ app/Http/Controllers/ImageController.php
@@ -10,10 +10,21 @@ class ImageController extends Controller
     //
     function index()
     {
-        $imgExt = new Imagick();
-        $imgExt->setResolution(300, 300);
-        $imgExt->readImage(public_path('acknowledgementSlip.pdf'));
-        $imgExt->writeImages('pdf_image_doc.jpg', true);
-        dd("Document has been converted");
+        // $imgExt = new Imagick();
+        // // $imgExt->setResolution(300, 300);
+        // $imgExt->readImage(public_path('secure_login.png'));
+        // $imgExt->writeImages('pdf_image_doc.jpg', true);
+        // dd("Document has been converted");
+
+        // $img = new Imagick();
+        // $img->readImage(public_path('secure_login.png'));
+        // $img->writeImages('pdf_image_doc.jpg', true);
+
+        $imagick = new Imagick();
+ // Reads image from PDF
+  $imagick->readImage('file.pdf');
+ // Writes an image
+ $imagick->writeImages('converted_page_one.jpg',false);
+
     }
 }
